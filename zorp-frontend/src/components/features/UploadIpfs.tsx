f2ac80bfe8466b4a6f77a625e68d31ee16b8d13e
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-02-25 18:29:34 +0000

    :warning: Partially implement expected actions...
    High-level outline of what _should_ happen:
    
    0. Get data from `<input>` file upload
    1. Get encryption public key from client
    2. Get encryption public key from `ZorpStudy.encryptionKey()`
    3. Encrypt file data with OpenPGP JS
    4. Upload encrypted file to IPFS
    5. Write IPFS pointer to blockchain via `ZorpStudy.submitData(ipfs_cid)`
    
    Where friction is felt most:
    
    - React wrappers `useReadContract` and `useWriteContract` from `wagmi`
       > ... smells of ye-olde XMLHttpRequest call-back hell
    - IPFS upload/pinning, solving will require either;
      - backend/lambda/clown-runner
      - or study creators to host IPFS pinning/auth infra
      - or a reasonably mature DeFi to IPFS solution
      - or integration with Filecoin, which would add to client requirements

--- zorp-frontend/src/components/features/UploadIpfs.tsx
+++ zorp-frontend/src/components/features/UploadIpfs.tsx
@@ -1,9 +1,31 @@
 // TODO: investigate why TypeScript no likes '@/lib/utils/openpgpUtils'
 import { openPgpEncrypt } from '../../lib/utils/openpgpUtils';
 
-import type { ChangeEvent } from 'react';
+import openpgp from 'openpgp';
+import { useEffect, useState } from 'react';
+import { readContract, writeContract } from '@wagmi/core';
+import { verifiedFetch } from '@helia/verified-fetch';
 
-export default function UploadIpfs({ className }: { className?: string }) {
+import { abi as ZorpStudyABI } from 'abi/IZorpStudy.json';
+
+import type { ChangeEvent } from 'react';
+import type { ResolvedRegister } from '@wagmi/core';
+import type { PublicKey } from 'openpgp';
+
+export default function UploadIpfs({
+	className,
+	config,
+}: {
+	className?: string;
+	config: {
+		wagmiConfig: ResolvedRegister['config'];
+		contracts: {
+			ZorpStudy: {
+				address: `0x${string}`;
+			};
+		};
+	};
+}) {
 	/**
 	 * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/files
 	 * @see https://github.com/microsoft/TypeScript/issues/31816
@@ -15,27 +37,55 @@ export default function UploadIpfs({ className }: { className?: string }) {
 		event.stopPropagation();
 		event.preventDefault();
 
+		const encryptionKeys: PublicKey[] = [];
+		// TODO: get GPG key for client from context/session/state-storage or generate one?
+		const gpgKeyClient = new openpgp.PublicKey();
+
+		// https://1.x.wagmi.sh/core/actions/readContract
+		const encryptionKey_cid = await readContract({
+			address: config.contracts.ZorpStudy.address,
+			abi: ZorpStudyABI,
+			functionName: 'encryptionKey',
+		});
+
+		const encryptionKey = await verifiedFetch(encryptionKey_cid).then(
+			(response) => {
+				if (!response.ok) {
+					throw new Error(`IPFS fetch failed to get -> ${encryptionKey_cid}`);
+				}
+				return response.text();
+			}
+		);
+
+		encryptionKeys.push(encryptionKey);
+
 		const file = event.target.files[0];
+		const text = await file.text();
+		const message = await openpgp.createMessage({ text });
+		const cypher_text = await openpgp.encrypt({
+			message,
+			encryptionKeys,
+			format: 'armored',
+		});
 
-		const plaintext = await file.text();
-		// TODO: get from context/session/state-storage
-		const publicKeysArmored = [];
+		const ipfs_cid = await someApiCallToIpfs(cypher_text);
 
-		const cypherText = await openPgpEncrypt(plaintext, publicKeysArmored);
-
-		// TODO: maybe convert `cypherText` into file-like blob
-		// TODO: submit encrypted data to IPFS
+		// https://1.x.wagmi.sh/core/actions/writeContract
+		// TODO: maybe notify devs of doc-rot?  Because source says two params are
+		//       needed to make writeContract happy
+		const result = await writeContract(config.wagmiConfig, {
+			address: config.contracts.ZorpStudy.address,
+			abi: ZorpStudyABI,
+			functionName: 'submitData',
+			args: [ipfs_cid],
+		});
 	};
 
 	return (
 		<>
 			<label>Upload</label>
 
-			<input
-				className={className}
-				type="file"
-				onChange={fileHandler}
-			/>
+			<input className={className} type="file" onChange={fileHandler} />
 		</>
 	);
 }
