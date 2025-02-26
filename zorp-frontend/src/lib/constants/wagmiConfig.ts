38f84cd92f64f3110fa2d946d74ee1725731032a
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-02-26 18:07:55 +0000

    Add hopefully correct configs for Anvil dev-net

--- zorp-frontend/src/lib/constants/wagmiConfig.ts
+++ zorp-frontend/src/lib/constants/wagmiConfig.ts
@@ -18,6 +18,9 @@ import {
   walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets';
 
+import { defineChain } from 'viem';
+import { chainConfig } from 'viem/op-stack';
+
 /**
  * WalletConnect Project ID
  * @description Required for all dApps using WalletConnect. Get your free projectId at
@@ -46,6 +49,11 @@ const RPC_URLS = {
     'https://base-rpc.publicnode.com', // Public Node
     'https://base.llamarpc.com', // Llama
   ],
+  ANVIL: [
+    'http://localhost:8545', // Default RPC
+    'http://localhost:8545', // Public Node
+    'http://localhost:8545', // Llama
+  ],
 } as const;
 
 /**
@@ -56,6 +64,7 @@ const transports = {
   [mainnet.id]: fallback(RPC_URLS.MAINNET.map((url) => http(url))),
   [arbitrum.id]: fallback(RPC_URLS.ARBITRUM.map((url) => http(url))),
   [base.id]: fallback(RPC_URLS.BASE.map((url) => http(url))),
+  [31337]: fallback(RPC_URLS.ANVIL.map((url) => http(url))),
 };
 
 //const { wallets } = getDefaultWallets();
@@ -86,6 +95,55 @@ const wallets: WalletList = [
   },
 ];
 
+export const anvil = /*#__PURE__*/ defineChain({
+	...chainConfig,
+	id: 31337,
+	name: 'Anvil',
+	nativeCurrency: { name: 'Woot', symbol: 'WAT', decimals: 18 },
+	rpcUrls: {
+		default: {
+			http: ['http://localhost:8545'],
+		},
+	},
+	blockExplorers: {
+		default: {
+			name: 'Basescan',
+			url: 'http://localhost:8545',
+			apiUrl: 'http://localhost:8545',
+		},
+	},
+	contracts: {
+		// ...chainConfig.contracts,
+		// disputeGameFactory: {
+		//   [sourceId]: {
+		//     address: '0x43edB88C4B80fDD2AdFF2412A7BebF9dF42cB40e',
+		//   },
+		// },
+		// l2OutputOracle: {
+		//   [sourceId]: {
+		//     address: '0x56315b90c40730925ec5485cf004d835058518A0',
+		//   },
+		// },
+		// multicall3: {
+		//   address: '0xca11bde05977b3631167028862be2a173976ca11',
+		//   blockCreated: 5022,
+		// },
+		// portal: {
+		//   [sourceId]: {
+		//     address: '0x49048044D57e1C92A77f79988d21Fa8fAF74E97e',
+		//     blockCreated: 17482143,
+		//   },
+		// },
+		// l1StandardBridge: {
+		//   [sourceId]: {
+		//     address: '0x3154Cf16ccdb4C6d922629664174b904d80F2C35',
+		//     blockCreated: 17482143,
+		//   },
+		// },
+	},
+	sourceId: 31337,
+});
+
 export const wagmiConfig = getDefaultConfig({
   appName: 'Next dApp Template',
   projectId: projectId,
@@ -94,6 +152,7 @@ export const wagmiConfig = getDefaultConfig({
     mainnet,
     arbitrum,
     base,
+    anvil,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   transports,
