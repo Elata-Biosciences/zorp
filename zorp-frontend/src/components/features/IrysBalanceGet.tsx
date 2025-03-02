8b9249a2680c9e45a923e2ed89fb75cad59d50e8
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-02 17:56:28 +0000

    Use `useCallback`
    Should resolve;
    
    - https://github.com/Elata-Biosciences/zorp/pull/3#discussion_r1976080887

--- zorp-frontend/src/components/features/IrysBalanceGet.tsx
+++ zorp-frontend/src/components/features/IrysBalanceGet.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { WebIrys } from '@irys/sdk';
 import type { BigNumber } from 'bignumber.js';
 import type { WebIrysOpts } from '@/@types/irys';
@@ -24,6 +24,42 @@ export default function IrysBalanceGet({
 }) {
 	const [message, setMessage] = useState<string>('Info: connected wallet/provider required');
 
+	const handleIrysBalanceGet = useCallback(async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		console.warn('IrysBalance', {event});
+
+		if (!address) {
+			const message = 'Info: waiting for client to connect wallet with an address';
+			console.warn('IrysBalance', {message, address});
+			setMessage(message);
+			return;
+		}
+
+		try {
+			const webIrys = await (new WebIrys(webIrysOpts)).ready();
+			const balance = await webIrys.getBalance(address);
+			console.warn('new WebIrys(webIrysOpts).ready().getLoadedBalance()', {message, balance});
+			setMessage(message);
+			setState(balance);
+		} catch (error: unknown) {
+			let message = 'Error: ';
+			if (!!error && typeof error == 'object') {
+				if ('message' in error) {
+					message += error.message;
+				} else if ('toString' in error) {
+					message += error.toString();
+				} else {
+					message += `Novel error detected -> ${error}`;
+				}
+			} else {
+				message += `Novel error detected -> ${error}`;
+			}
+
+			console.error('new WebIrys(webIrysOpts).ready() ...', {message, error});
+			setMessage(message);
+			setState(null);
+		}
+	}, [address]);
+
 	return (
 		<>
 			<label className={`irys_balance irys_balance__label ${className}`}>{labelText}</label>
@@ -33,59 +69,7 @@ export default function IrysBalanceGet({
 				onClick={(event) => {
 					event.stopPropagation();
 					event.preventDefault();
-
-					console.warn('IrysBalance', {event});
-
-					if (!address) {
-						const message = 'Info: waiting for client to connect wallet with an address';
-						console.warn('IrysBalance', {message, address});
-						setMessage(message);
-						return;
-					}
-
-					try {
-						new WebIrys(webIrysOpts)
-							.ready()
-							.then((webIrys) => {
-								const message = 'Info: attempting Irys get loaded balance';
-								console.warn('new WebIrys(webIrysOpts).ready()', {message, webIrys, webIrysOpts});
-								setMessage(message);
-								return webIrys.getBalance(address);
-							}).then((balance) => {
-								const message = `Success: got Irys balance of: ${balance}`;
-								console.warn('new WebIrys(webIrysOpts).ready().getLoadedBalance()', {message, balance});
-								setMessage(message);
-								setState(balance);
-							}).catch((error) => {
-								let message = '';
-								if ('message' in error) {
-									message = error.message;
-								} else {
-									message = error.toString();
-								}
-
-								console.error('new WebIrys(webIrysOpts).ready() ...', {message, error});
-								setMessage(message);
-								setState(null);
-							});
-					} catch (error: unknown) {
-						let message = 'Error: ';
-						if (!!error && typeof error == 'object') {
-							if ('message' in error) {
-								message += error.message;
-							} else if ('toString' in error) {
-								message += error.toString();
-							} else {
-								message += `Novel error detected -> ${error}`;
-							}
-						} else {
-							message += `Novel error detected -> ${error}`;
-						}
-
-						console.error('new WebIrys(webIrysOpts).ready() ...', {message, error});
-						setMessage(message);
-						setState(null);
-					}
+					handleIrysBalanceGet(event);
 				}}
 			>Get Irys balance</button>
 
