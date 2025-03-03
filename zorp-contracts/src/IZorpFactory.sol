b3e8fb8520be64163e0cc40cfd5ae355cb3b6afd
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-03 17:35:37 +0000

    Add doc-comments and on-chain examples...
    Still need to add off-chain examples for `IZorpFactory` as well as
    on/off-chain examples for `IZorpStudy`/`ZorpStudy` contracts

--- zorp-contracts/src/IZorpFactory.sol
+++ zorp-contracts/src/IZorpFactory.sol
@@ -21,10 +21,49 @@ interface IZorpFactory_Storage {
 /// @title Executable logic for `ZorpFactory`
 interface IZorpFactory_Functions {
     /* Public {{{ */
+        /// @notice Deploy a new `ZorpStudy` contract and return its address
+        /// @param initialOwner Address of owner for study, I.E. the account allowed to execute functions like; `.flagInvalidSubmission(address)`, `.startStudy()`, and `.endStudy()`
+        /// @param encryptionKey pointer to public GPG/PGP key
+        /// @return Address of new `ZorpStudy` contract
+        ///
+        /// @dev see `./IZorpStudy.sol`
+        /// @dev see `./ZorpStudy.sol` → `constructor`
+        ///
+        /// ## On-chain example
+        ///
+        /// ```solidity
+        /// import { IZorpFactory } from "<NAME_SPACE>/src/IZorpFactory.sol";
+        ///
+        /// address factory = <ADDRESS_OF_ZORP_FACTORY>;
+        ///
+        /// address payable initialOwner = "<YOUR_PUBLIC_WALLET_ADDRESS>";
+        /// string memory encryptionKey = "<CID_TO_YOUR_PUBLIC_PGP_KEY>";
+        ///
+        /// address newStudy = IZorpFactory(factory).createStudy{value: 1 ether}(initialOwner, encryptionKey);
+        /// /* ... Make use of `IZorpStudy(newStudy)` features ... */
+        /// ```
+        ///
+        /// @custom:throws "ZorpStudy: Invalid message value"
         function createStudy(address payable initialOwner, string memory encryptionKey) external payable returns (address);
     /* Public }}} */
 
     /* Owner {{{ */
+        /// @notice Restricted to `IZorpFactory.owner()`
+        /// @param to Address that should be paid from this contract with the native currency used by Blockchain
+        /// @param amount Native currency for Blockchain to transfer from this contract to recipient
+        ///
+        /// ## On-chain example
+        ///
+        /// ```solidity
+        /// address factory = <ADDRESS_OF_ZORP_FACTORY>;
+        ///
+        /// address payable to = <ADDRESS_TO_PAY>;
+        /// uint256 amount = 1 ether;
+        ///
+        /// IZorpFactory(factory).withdraw(to, amount);
+        /// ```
+        ///
+        /// @custom:throws "ZorpFactory: Failed withdraw"
         function withdraw(address payable to, uint256 amount) external payable;
     /* Owner }}} */
 
@@ -37,9 +76,14 @@ interface IZorpFactory_Functions {
     /* Viewable }}} */
 }
 
+/// @title Organize any inherited contracts that also contain publicly accessible functions/storage
 interface IZorpFactory_Inherited is IOwnable {}
 
 /// @title On/Off chain consumers of `ZorpStudy` may wish to use this interface
+/// @author wkyleg
+/// @author S0AndS0
+/// @custom:link https://elata.bio/
+/// @custom:link https://github.com/Elata-Biosciences/zorp
 ///
 /// ## On-chain example
 ///
@@ -55,4 +99,8 @@ interface IZorpFactory_Inherited is IOwnable {}
 ///     }
 /// }
 /// ```
+///
+/// @custom:file ./ZorpFactory.sol
+/// @custom:link https://elata.bio/
+/// @custom:link https://github.com/Elata-Biosciences/zorp
 interface IZorpFactory is IZorpFactory_Storage, IZorpFactory_Functions, IZorpFactory_Inherited {}
