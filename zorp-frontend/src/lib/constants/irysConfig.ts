fe908b59e4c989c99f008aa0db76ce647953ddb6
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-06 17:07:07 +0000

    :warning: Finish moving shared Irys configs

--- zorp-frontend/src/lib/constants/irysConfig.ts
+++ zorp-frontend/src/lib/constants/irysConfig.ts
@@ -1,8 +1,18 @@
 
+import { BigNumber } from 'bignumber.js';
+import type { WebIrysOpts } from '@/@types/irys';
+
 /**
  * @see https://arweave-tools.irys.xyz/overview/downloading
  */
 export const gatewayUrl = {
 	'arweave': 'https://arweave.net',
 	'irys': 'https://gateway.irys.xyz',
-};
+} as const;
+
+// TODO: define correct token, and other options too
+export const webIrysOpts: WebIrysOpts = {
+	token: 'WAT'
+} as const;
+
+export const irysBalanceThreshold = BigNumber(0.1);
