311a5bdc85b7592f2f458e90178c5d33118f52d5
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-06 22:17:20 +0000

    :tada: Make better use of `forge` features...
    Now example studies are created via `DeployAnvil` smart contract, which
    significantly reduces the Bash script wrapping requirements :-)

--- scripts/zorp-contracts-publish-to-anvil.sh
+++ scripts/zorp-contracts-publish-to-anvil.sh
@@ -2,7 +2,6 @@
 
 ##
 # WARN: not intended for CI/CD or public build/deploy pipelines!!!
-#       because when __VERBOSE__ is truthy private keys are exposed
 ##
 
 ## Halt and catch fire on errors
@@ -16,169 +15,20 @@ done
 __DIR__="$(cd -P "$(dirname "${__SOURCE__}")" && pwd)";
 __G_DIR__="$(dirname "${__DIR__}")";
 
-## Read environment variable(s)
-__VERBOSE__="${VERBOSE:-0}";
-_encryptionKey="${1:?Undefined GPG public encryption key}";
-
 ## Some variables for Anvil
 _test_net_url='127.0.0.1:8545';
 _test_private_key0='0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 _initialOwner='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 
-_study_initialOwners=(
-	'0xd6c12888363B422e72647FB95c97fE007C8a7870'
-	'0x612952ab356E692723100b9e91d7cCA209bB4D69'
-	'0x3A5b8058b73d6F93b71A46a697Ea35f165bc72A8'
-	'0xd50dAfCdB28687B78182566118A53B45e54298e7'
-	'0xA6606c82e02337Bd952de5db38104569134B95bd'
-	'0x0e9A6f6aBb4a46767515F07d15229755Ac16BFbf'
-	'0x1ebE2DC576b1320f8a20a6f4D50Aa9FA183436b2'
-	'0x021542581584b215947efaED00778F79b2e8Fb6D'
-	'0xF4eF5E0f51dA631Ea5fCc7AB2Cb1F98503f571C3'
-)
+pushd "${__G_DIR__}/zorp-contracts" 1>/dev/null 2>&1;
 
-##
-# Publish `ZorpFactory` to Anvil and return contract address
-ZorpFactory_constructor() {
-	local -a _args=(
-		create
-		--broadcast
-		--rpc-url "${_test_net_url}"
-		--private-key "${_test_private_key0}"
-		src/ZorpFactory.sol:ZorpFactory
-		--constructor-args "${_initialOwner}"
-	);
+forge script \
+	-vvvvv \
+	--offline \
+	--broadcast \
+	--rpc-url "${_test_net_url}" \
+	--private-key "${_test_private_key0}" \
+	src/DeployAnvil.sol:DeployAnvil;
 
-	pushd "${__G_DIR__}/zorp-contracts" 1>/dev/null 2>&1;
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpFactory_publish command --->\nforge %s\n<--- ZorpFactory_publish command\n' "${_args[*]}";
-	fi
-
-	local _result;
-	_result="$( forge "${_args[@]}" )";
-	popd 1>/dev/null 2>&1;
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpFactory_publish result --->\n%s\n<--- ZorpFactory_publish result\n' "${_result:?Undefined result}";
-	fi
-
-	awk '{
-		if ($0 ~ "Deployed to:") {
-			print $3;
-			exit 0;
-		}
-	}' <<<"${_result}";
-}
-
-ZorpFactory_owner() {
-	local _zorp_factory_address="${1:?Undefined Zorp Factory address}";
-
-	local _args=(
-		call "${_zorp_factory_address}"
-		--rpc-url "${_test_net_url}"
-		'owner()(address)'
-	);
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpFactory_owner command --->\ncast %s\n<--- ZorpFactory_owner command\n' "${_args[*]}";
-	fi
-
-	cast "${_args[@]}";
-}
-
-ZorpFactory_paginateStudies() {
-	local _zorp_factory_address="${1:?Undefined Zorp Factory address}";
-
-	local _args=(
-		call "${_zorp_factory_address}"
-		--rpc-url "${_test_net_url}"
-		'paginateStudies(uint256,uint256)(address[])'
-			"1"
-			"${#_study_initialOwners[@]}"
-	);
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpFactory_paginateStudies command --->\ncast %s\n<--- ZorpFactory_paginateStudies command\n' "${_args[*]}";
-	fi
-
-	cast "${_args[@]}";
-}
-
-##
-#
-ZorpFactory_createStudy() {
-	local _zorp_factory_address="${1:?Undefined Zorp Factory address}";
-	local _study_initialOwner="${2:?Undefined Zorp Study owner address}";
-	local _study_encryptionKey="${3:?Undefined Zorp Study encryption key}";
-
-	local _args=(
-		call "${_zorp_factory_address}"
-		--rpc-url "${_test_net_url}"
-		--private-key "${_test_private_key0}"
-		--value "1ether"
-		'createStudy(address,string)(address)'
-			"${_study_initialOwner}"
-			"${_study_encryptionKey}"
-	);
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpFactory_createStudy command --->\ncast %s\n<--- ZorpFactory_createStudy command\n' "${_args[*]}";
-	fi
-
-	cast "${_args[@]}";
-}
-
-##
-# Publish `ZorpStudy` to Anvil and return contract address
-ZorpStudy_constructor() {
-	local -a _args=(
-		create
-		--broadcast
-		--rpc-url "${_test_net_url}"
-		--private-key "${_test_private_key0}"
-		src/ZorpStudy.sol:ZorpStudy
-		--value "1ether"
-		--constructor-args "${_initialOwner}" "${_encryptionKey}"
-	);
-
-	pushd "${__G_DIR__}/zorp-contracts" 1>/dev/null 2>&1;
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpStudy_publish command --->\nforge %s\n<--- ZorpStudy_publish command\n' "${_args[*]}";
-	fi
-
-	local _result;
-	_result="$( forge "${_args[@]}" )";
-	popd 1>/dev/null 2>&1;
-
-	if ((__VERBOSE__)); then
-		printf >&2 'ZorpStudy_publish result --->\n%s\n<--- ZorpStudy_publish result\n' "${_result:?Undefined result}";
-	fi
-
-	awk '{
-		if ($0 ~ "Deployed to:") {
-			print $3;
-			exit 0;
-		}
-	}' <<<"${_result}";
-}
-
-## Do the thangs
-_zorp_factory_address="$( ZorpFactory_constructor )";
-printf >&2 '_zorp_factory_address -> %s\n' "${_zorp_factory_address}"
-
-_zorp_study_addresses=();
-for i in "${!_study_initialOwners[@]}"; do
-	_study_initialOwner="${_study_initialOwners[${i}]}";
-	_zorp_study_address="$( ZorpFactory_createStudy "${_zorp_factory_address}" "${_study_initialOwner}" "${i}" )";
-	printf >&2 'i -> %i | _study_initialOwner -> %s | _zorp_study_address -> %s\n' "${i}" "${_study_initialOwner}" "${_zorp_study_address}";
-	_zorp_study_addresses+=( "${_zorp_study_address}" );
-done
-
-_zorp_studies="$( ZorpFactory_paginateStudies "${_zorp_factory_address}" )";
-printf >&2 '_zorp_studies -> %s\n' "${_zorp_studies}"
-
-_zorp_study_address="$( ZorpStudy_constructor )";
-printf >&2 '_zorp_study_address -> %s\n' "${_zorp_study_address}"
+popd 1>/dev/null 2>&1;
 
---
+++ zorp-contracts/src/DeployAnvil.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../lib/forge-std/src/Script.sol";
+
+import { ZorpFactory } from "./ZorpFactory.sol";
+import { IZorpFactory } from "./IZorpFactory.sol";
+
+/// ## Example `forge` usage
+///
+/// ```bash
+/// _test_net_url='127.0.0.1:8545';
+/// _test_private_key0='0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+///
+/// forge script -vvvvv \
+///   --broadcast \
+///   --rpc-url "${_test_net_url}" \
+///   --private-key "${_test_private_key0}" \
+///   src/DeployAnvil.sol:DeployAnvil;
+/// ```
+///
+/// ## Expected results if running on fresh Anvil node
+///
+/// ```
+/// ZorpFactory:  0x5FbDB2315678afecb367f032d93F642f64180aa3
+/// ZorpStudy:  0xa16E02E87b7454126E5E10d957A927A7F5B5d2be
+/// ZorpStudy:  0xB7A5bd0345EF1Cc5E66bf61BdeC17D2461fBd968
+/// ZorpStudy:  0xeEBe00Ac0756308ac4AaBfD76c05c4F3088B8883
+/// ZorpStudy:  0x10C6E9530F1C1AF873a391030a1D9E8ed0630D26
+/// ZorpStudy:  0x603E1BD79259EbcbAaeD0c83eeC09cA0B89a5bcC
+/// ZorpStudy:  0x86337dDaF2661A069D0DcB5D160585acC2d15E9a
+/// ZorpStudy:  0x9CfA6D15c80Eb753C815079F2b32ddEFd562C3e4
+/// ZorpStudy:  0x427f7c59ED72bCf26DfFc634FEF3034e00922DD8
+/// ZorpStudy:  0x275039fc0fd2eeFac30835af6aeFf24e8c52bA6B
+/// ```
+///
+/// @custom:link https://github.com/foundry-rs/foundry/blob/54af8693b8fcc7a3f65136c1188e89661155955d/crates/cast/tests/cli/main.rs#L419
+/// @custom:link https://book.getfoundry.sh/reference/anvil/
+/// @custom:link https://github.com/foundry-rs/foundry/issues/2519
+contract DeployAnvil is Script {
+    address payable public constant ZorpFactory_Owner = payable(address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
+
+    address[] public ZorpStudy_OwnerList = [
+        address(0xd6c12888363B422e72647FB95c97fE007C8a7870),
+        address(0x612952ab356E692723100b9e91d7cCA209bB4D69),
+        address(0x3A5b8058b73d6F93b71A46a697Ea35f165bc72A8),
+        address(0xd50dAfCdB28687B78182566118A53B45e54298e7),
+        address(0xA6606c82e02337Bd952de5db38104569134B95bd),
+        address(0x0e9A6f6aBb4a46767515F07d15229755Ac16BFbf),
+        address(0x1ebE2DC576b1320f8a20a6f4D50Aa9FA183436b2),
+        address(0x021542581584b215947efaED00778F79b2e8Fb6D),
+        address(0xF4eF5E0f51dA631Ea5fCc7AB2Cb1F98503f571C3)
+    ];
+
+    // Warn: the following are not at all correct!
+    string[] public ZorpStudy_GpgCidList = [
+        "0xd6c12888363B422e72647FB95c97fE007C8a7870",
+        "0x612952ab356E692723100b9e91d7cCA209bB4D69",
+        "0x3A5b8058b73d6F93b71A46a697Ea35f165bc72A8",
+        "0xd50dAfCdB28687B78182566118A53B45e54298e7",
+        "0xA6606c82e02337Bd952de5db38104569134B95bd",
+        "0x0e9A6f6aBb4a46767515F07d15229755Ac16BFbf",
+        "0x1ebE2DC576b1320f8a20a6f4D50Aa9FA183436b2",
+        "0x021542581584b215947efaED00778F79b2e8Fb6D",
+        "0xF4eF5E0f51dA631Ea5fCc7AB2Cb1F98503f571C3"
+    ];
+
+    function run() external {
+        vm.startBroadcast();
+
+        // vm.createSelectFork(vm.rpcUrl("anvil"));
+        address factory = address(new ZorpFactory(ZorpFactory_Owner));
+        console2.log("ZorpFactory: ", factory);
+
+        for (uint256 i; i < ZorpStudy_OwnerList.length;) {
+            address study = address(IZorpFactory(factory).createStudy{value: 1 ether}(payable(ZorpStudy_OwnerList[i]), ZorpStudy_GpgCidList[i]));
+            console2.log("ZorpStudy: ", study);
+            unchecked { ++i; }
+        }
+
+        vm.stopBroadcast();
+    }
+}
+
