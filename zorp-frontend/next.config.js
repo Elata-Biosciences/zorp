93f7e66e1b6540da84124fdc103263dcc1507552
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-06 02:33:54 +0000

    :warning: Start transitioning JSON to TS for ABIs

--- zorp-frontend/next.config.js
+++ zorp-frontend/next.config.js
@@ -2,6 +2,7 @@ const merge = require('lodash.merge');
 const fs = require('fs');
 const process = require('process');
 const path = require('path');
+const { foundry } = require('@wagmi/cli/plugins');
 
 /**
  * @see https://nextjs.org/docs/pages/building-your-application/deploying#static-html-export
@@ -26,30 +27,52 @@ async function hook(phase, { nextConfigCustom }) {
 	const nextConfig = merge({}, nextConfigDefaults, nextConfigCustom);
 	console.log('START -- next.config.js -- hook(phase, { nextConfigCustom }) ->', { phase, nextConfigCustom, nextConfig });
 
+	/**
+	 * @see https://d.sh/cli/getting-started
+	 */
+	const foundryResult = foundry({
+		project: path.join(path.dirname(__dirname), 'zorp-contracts'),
+		include: [
+			'IZorpFactory.json',
+			'IZorpStudy.json',
+		],
+	});
+	const contractOutDir = path.join(__dirname, 'src', 'lib', 'constants', 'wagmiContractConfig');
+	for (const contract of await foundryResult.contracts()) {
+		const contractPathDest = path.join(contractOutDir, `${contract.name}.ts`);
+		if (fs.existsSync(contractPathDest)) {
+			console.warn(`Output contract config file already exists ->`, {contractPathDest});
+			continue;
+		}
+
+		const contractOutputText = `export const ${contract.name} = ${JSON.stringify(contract, null, 2)} as const;`;
+		fs.writeFileSync(contractPathDest, contractOutputText, { encoding: 'utf-8', mode: '440' });
+	}
+
 	// Copy select ABIs from `../zorp-contracts/out/` to `public/assets/abi/`
 	//
 	// Results in `../zorp-contracts/out/IZorpFactory.sol/IZorpFactory.json`
 	//            `public/assets/abi/IZorpFactory.json`
 	//            integration
-	const abiOutDir = path.join(process.cwd(), 'public', 'assets', 'abi');
-	if (!fs.existsSync(abiOutDir)) {
-		fs.mkdirSync(abiOutDir, { recursive: true });
-	}
-	const abiNames = [ 'IZorpFactory', 'IZorpStudy' ];
-	for (const abiName of abiNames) {
-		const abiPathSource = path.join(path.dirname(process.cwd()), 'zorp-contracts', 'out', `${abiName}.sol`, `${abiName}.json`);
-		const abiPathDest = path.join(abiOutDir, `${abiName}.json`);
+	// const abiOutDir = path.join(process.cwd(), 'public', 'assets', 'abi');
+	// if (!fs.existsSync(abiOutDir)) {
+	// 	fs.mkdirSync(abiOutDir, { recursive: true });
+	// }
+	// const abiNames = [ 'IZorpFactory', 'IZorpStudy' ];
+	// for (const abiName of abiNames) {
+	// 	const abiPathSource = path.join(path.dirname(process.cwd()), 'zorp-contracts', 'out', `${abiName}.sol`, `${abiName}.json`);
+	// 	const abiPathDest = path.join(abiOutDir, `${abiName}.json`);
 
-		if (!fs.existsSync(abiPathSource)) {
-			throw new Error(`Missing: ${abiPathSource}\n\tDid you forget -> forge build`);
-		}
-		if (fs.existsSync(abiPathDest)) {
-			console.warn(`Output ABI file already exists ->`, {abiPathDest});
-			continue;
-		}
+	// 	if (!fs.existsSync(abiPathSource)) {
+	// 		throw new Error(`Missing: ${abiPathSource}\n\tDid you forget -> forge build`);
+	// 	}
+	// 	if (fs.existsSync(abiPathDest)) {
+	// 		console.warn(`Output ABI file already exists ->`, {abiPathDest});
+	// 		continue;
+	// 	}
 
-		fs.copyFileSync(abiPathSource, abiPathDest);
-	}
+	// 	fs.copyFileSync(abiPathSource, abiPathDest);
+	// }
 
 	// Detect if deploying to GitHub Pages
 	/** @type {string|undefined} */
