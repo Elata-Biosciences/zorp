cc390d03ef828ed2f8e19c460eab60fe13d78e60
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-15 22:23:23 +0000

    Abstract abstractions to wrap with wrapper

--- zorp-frontend/src/lib/utils/irys.ts
+++ zorp-frontend/src/lib/utils/irys.ts
@@ -1,14 +1,16 @@
-// 'use client';
-
 import * as openpgp from 'openpgp';
 import * as irysConfig from '@/lib/constants/irysConfig';
+
+export async function getIrysResponseFromCid(cid: string) {
+	const url = `${irysConfig.gatewayUrl.irys}/ipfs/${cid}`;
+	return await fetch(url);
+}
 
 /**
  * Wrapper to aid mocked testing and reduce run-time boilerplate
  */
 export async function getGpgKeyFromCid(cid: string) {
-	const url = `${irysConfig.gatewayUrl.irys}/ipfs/${cid}`;
-	const response = await fetch(url);
+	const response = await getIrysResponseFromCid(cid);
 	if (response.ok) {
 		const text = await response.text();
 		const key = await openpgp.readKey({ armoredKey: text });
