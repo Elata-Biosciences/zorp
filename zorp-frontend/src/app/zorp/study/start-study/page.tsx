d5c87c91a3239ff6c573aa1f3354ababc1f5593e
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-17 21:42:02 +0000

    Use `useCallback` for input and button events

--- zorp-frontend/src/app/zorp/study/start-study/page.tsx
+++ zorp-frontend/src/app/zorp/study/start-study/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useId, useState } from 'react';
+import { useCallback, useId, useState } from 'react';
 import { useAccount, useReadContract, useWriteContract } from 'wagmi';
 import { useContracts } from '@/contexts/Contracts';
 import ThemeSwitch from '@/components/features/ThemeSwitch';
@@ -67,6 +67,58 @@ export default function ZorpStudyWriteStartStudy() {
 								&& assertsBlockchain.isStudyOwner
 								&& assertsBlockchain.isStudyInactive;
 
+	const handleChangeStudyAddress = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+		setAddressStudy(event.target.value as `0x${string}`);
+	}, [ setAddressStudy ]);
+
+	const handleOnClick = useCallback(async () => {
+		if (!enabled) {
+			return;
+		}
+
+		setIsFetching(true);
+
+		try {
+			const response = await writeContractAsync({
+				abi: IZorpStudy.abi,
+				address: IZorpStudy.address,
+				functionName: 'startStudy',
+				args: [],
+			});
+
+			if (!!response) {
+				setReceipt(response);
+			} else {
+				setReceipt(`...  error with receipt response -> ${response}`);
+			}
+		} catch (error) {
+			let message = 'Error: ';
+			if (!!error && typeof error == 'object') {
+				if ('message' in error) {
+					message += error.message;
+				} else if ('toString' in error) {
+					message += error.toString();
+				} else {
+					message += `Novel error detected -> ${error}`;
+				}
+			} else {
+				message += `Novel error detected -> ${error}`;
+			}
+
+			console.error('ZorpStudyWriteStartStudy ->', { message, error });
+			setReceipt(message);
+			return error;
+		} finally {
+			setIsFetching(false);
+		}
+	}, [
+		IZorpStudy,
+		enabled,
+		setIsFetching,
+		setReceipt,
+		writeContractAsync,
+	]);
+
 	return (
 		<div className="w-full flex flex-col">
 			<h1 className="flex flex-col sm:flex-row justify-center items-center text-4xl font-bold">
@@ -80,44 +132,15 @@ export default function ZorpStudyWriteStartStudy() {
 			<input
 				id={addressStudyId}
 				value={addressStudy}
-				onChange={(event) => {
-					setAddressStudy(event.target.value as `0x${string}`);
-				}}
+				onChange={handleChangeStudyAddress}
 				disabled={disabled}
 			/>
 
 			<button
-				onClick={(event) => {
+				onClick={async (event) => {
 					event.preventDefault();
 					event.stopPropagation();
-
-					if (!enabled) {
-						console.warn('Missing required state', {
-							isConnected,
-							assertsClient,
-							assertsBlockchain,
-						});
-						return;
-					}
-
-					setIsFetching(true);
-					writeContractAsync({
-						abi: IZorpStudy.abi,
-						address: IZorpStudy.address,
-						functionName: 'startStudy',
-						args: [],
-					}).then((response) => {
-						if (!!response) {
-							setReceipt(response);
-						} else {
-							setReceipt(`...  error with receipt response -> ${response}`);
-						}
-					}).catch((error) => {
-						console.error(error);
-						setReceipt(`...  error with writeContractAsync error -> ${error}`);
-					}).finally(() => {
-						setIsFetching(false);
-					});
+					await handleOnClick();
 				}}
 				disabled={disabled}
 			>Start {enabled ? 'Available' : 'unavailable'}</button>
