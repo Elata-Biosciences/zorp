cdc2aa8bf3cee5a2f0a8b489a623bb2ee7f115bf
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-17 21:11:06 +0000

    Use `useCallback` for address change and button

--- zorp-frontend/src/app/zorp/study/encryption-key-cid/page.tsx
+++ zorp-frontend/src/app/zorp/study/encryption-key-cid/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useId, useState } from 'react';
+import { useCallback, useId, useState } from 'react';
 import { useReadContract } from 'wagmi';
 import { useContracts } from '@/contexts/Contracts';
 import ThemeSwitch from '@/components/features/ThemeSwitch';
@@ -31,6 +31,21 @@ export default function ZorpStudyReadEncryptionKeyCid() {
 		},
 	});
 
+	const handleChangeStudyAddress = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+		setAddressStudy(event.target.value as `0x${string}`);
+	}, [ setAddressStudy ]);
+
+	const handleOnClick = useCallback(async () => {
+		if (!enabled) {
+			return;
+		}
+
+		await refetch();
+	}, [
+		enabled,
+		refetch,
+	]);
+
 	return (
 		<div className="w-full flex flex-col">
 			<h1 className="flex flex-col sm:flex-row justify-center items-center text-4xl font-bold">
@@ -44,25 +59,15 @@ export default function ZorpStudyReadEncryptionKeyCid() {
 			<input
 				id={addressStudyId}
 				value={addressStudy}
-				onChange={(event) => {
-					setAddressStudy(event.target.value as `0x${string}`);
-					console.warn('ZorpStudyReadEncryptionKeyCid input ->', {  enabled, addressStudy });
-				}}
+				onChange={handleChangeStudyAddress}
 				disabled={isFetching}
 			/>
 
 			<button
-				onClick={(event) => {
+				onClick={async (event) => {
 					event.stopPropagation();
 					event.preventDefault();
-					console.warn('ZorpStudyReadEncryptionKeyCid button ->', { enabled, addressStudy });
-					if (!enabled) {
-						console.warn('ZorpStudyReadEncryptionKeyCid button ->', { enabled });
-						return;
-					}
-					console.warn('ZorpStudyReadEncryptionKeyCid button ->', { event });
-
-					refetch();
+					await handleOnClick()
 				}}
 			>Request GPG key CID</button>
 
