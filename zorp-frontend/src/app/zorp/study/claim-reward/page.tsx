d14bb30ee570c52936843a4dc8c1b2a8b2225a93
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-17 21:05:13 +0000

    Memoize state variables and callback functions

--- zorp-frontend/src/app/zorp/study/claim-reward/page.tsx
+++ zorp-frontend/src/app/zorp/study/claim-reward/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useId, useState } from 'react';
+import { useCallback, useId, useMemo, useState } from 'react';
 import { useAccount, useReadContract, useWriteContract } from 'wagmi';
 import { useContracts } from '@/contexts/Contracts';
 import ThemeSwitch from '@/components/features/ThemeSwitch';
@@ -19,11 +19,18 @@ export default function ZorpStudyWriteClaimReward() {
 	const { address, isConnected } = useAccount();
 	const { writeContractAsync } = useWriteContract();
 
-	const assertsClient = {
-		isAddressParticipantSet: address?.length === addressStudyAnvil.length && address.startsWith('0x'),
-		isAddressStudySet: addressStudy.length === addressStudyAnvil.length && addressStudy.startsWith('0x'),
-		isContractStudySet: !!IZorpStudy?.abi && !!Object.keys(IZorpStudy.abi).length && !!IZorpStudy?.address.length,
-	};
+	const assertsClient = useMemo(() => {
+		return {
+			isAddressParticipantSet: address?.length === addressStudyAnvil.length && address.startsWith('0x'),
+			isAddressStudySet: addressStudy.length === addressStudyAnvil.length && addressStudy.startsWith('0x'),
+			isContractStudySet: !!IZorpStudy?.abi && !!Object.keys(IZorpStudy.abi).length && !!IZorpStudy?.address.length,
+		};
+	}, [
+		IZorpStudy,
+		address,
+		addressStudy,
+		addressStudyAnvil,
+	]);
 
 	const { data: participant_status, isFetching: isFetchingParticipantStatus } = useReadContract({
 		abi: IZorpStudy.abi,
@@ -48,10 +55,15 @@ export default function ZorpStudyWriteClaimReward() {
 		},
 	});
 
-	const assertsBlockchain = {
-		isParticipantSubmitted: participant_status == 1,
-		isStudyFinised: study_status == 2,
-	};
+	const assertsBlockchain = useMemo(() => {
+		return {
+			isParticipantSubmitted: participant_status == 1,
+			isStudyFinised: study_status == 2,
+		};
+	}, [
+		participant_status,
+		study_status,
+	]);
 
 	const disabled = isFetching
 								|| isFetchingParticipantStatus 
@@ -64,6 +76,60 @@ export default function ZorpStudyWriteClaimReward() {
 								&& assertsClient.isContractStudySet
 								&& assertsBlockchain.isParticipantSubmitted
 								&& assertsBlockchain.isStudyFinised;
+
+	const handleChangePaginateStudyAddress = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+		setAddressStudy(event.target.value as `0x${string}`);
+	}, [ setAddressStudy ]);
+
+	const handleZorpStudyWriteClaimReward = useCallback(async () => {
+		if (!enabled) {
+			console.warn('Missing required state', { assertsClient, assertsBlockchain });
+			return;
+		}
+
+		setIsFetching(true);
+
+		try {
+			const response = await writeContractAsync({
+				abi: IZorpStudy.abi,
+				address: IZorpStudy.address,
+				functionName: 'claimReward',
+				args: [],
+			});
+
+			if (!!response) {
+				setReceipt(response);
+			} else {
+				setReceipt(`...  error with receipt response -> ${response}`);
+			}
+		} catch (error) {
+			let message = 'Error: ';
+			if (!!error && typeof error == 'object') {
+				if ('message' in error) {
+					message += error.message;
+				} else if ('toString' in error) {
+					message += error.toString();
+				} else {
+					message += `Novel error detected -> ${error}`;
+				}
+			} else {
+				message += `Novel error detected -> ${error}`;
+			}
+
+			console.error('ZorpStudyWriteClaimReward ...', { message, error });
+			setReceipt(message);
+			return error;
+		} finally {
+			setIsFetching(false);
+		}
+	}, [
+		IZorpStudy,
+		assertsBlockchain,
+		assertsClient,
+		enabled,
+		setIsFetching,
+		writeContractAsync,
+	]);
 
 	return (
 		<div className="w-full flex flex-col">
@@ -78,40 +144,15 @@ export default function ZorpStudyWriteClaimReward() {
 			<input
 				id={addressStudyId}
 				value={addressStudy}
-				onChange={(event) => {
-					setAddressStudy(event.target.value as `0x${string}`);
-				}}
+				onChange={handleChangePaginateStudyAddress}
 				disabled={disabled}
 			/>
 
 			<button
-				onClick={(event) => {
+				onClick={async (event) => {
 					event.preventDefault();
 					event.stopPropagation();
-
-					if (!enabled) {
-						console.warn('Missing required state', { assertsClient, assertsBlockchain });
-						return;
-					}
-
-					setIsFetching(true);
-					writeContractAsync({
-						abi: IZorpStudy.abi,
-						address: IZorpStudy.address,
-						functionName: 'claimReward',
-						args: [],
-					}).then((response) => {
-						if (!!response) {
-							setReceipt(response);
-						} else {
-							setReceipt(`...  error with receipt response -> ${response}`);
-						}
-					}).catch((error) => {
-						console.error(error);
-						setReceipt(`...  error with writeContractAsync error -> ${error}`);
-					}).finally(() => {
-						setIsFetching(false);
-					});
+					await handleZorpStudyWriteClaimReward();
 				}}
 				disabled={disabled}
 			>Claim Reward {enabled ? 'Available' : 'unavailable'}</button>
