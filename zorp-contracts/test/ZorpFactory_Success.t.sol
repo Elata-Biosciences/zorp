1d5f2433cd91056876a94799cfbc4acf18cc946d
Digital-Mercenaries <S0AndS0@digital-mercenaries.com>
2025-03-24 20:11:28 +0000

    Add tests for contract _happy paths_

---
+++ zorp-contracts/test/ZorpFactory_Success.t.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { Test } from "forge-std/Test.sol";
+
+import { ZorpFactory } from "../src/ZorpFactory.sol";
+import {
+    IZorpFactory,
+    FactoryUpdated,
+    StudyCreated
+} from "../src/IZorpFactory.sol";
+
+
+contract ZorpFactory_Success_Read_Test is Test {
+    address payable immutable ZORP_FACTORY__OWNER;
+    address ref_factory;
+
+    constructor() {
+        ZORP_FACTORY__OWNER = payable(address(this));
+    }
+
+    function setUp() public {
+        vm.deal(address(this), 1000 ether);
+        // Deploy a fresh factory for each test
+        ref_factory = address(new ZorpFactory(ZORP_FACTORY__OWNER));
+    }
+
+    function test_read__VERSION() public view {
+        uint256 expected = 1;
+        uint256 got = IZorpFactory(ref_factory).VERSION();
+        vm.assertEq(expected, got, "Unexpected: `ZorpFactory.VERSION()`");
+    }
+
+    function test_read__latest_study_index() public view {
+        uint256 expected = 0;
+        uint256 got = IZorpFactory(ref_factory).latest_study_index();
+        vm.assertEq(expected, got, "Unexpected: `ZorpFactory.latest_study_index()`");
+    }
+
+    function test_read__studies() public view {
+        address expected = address(0);
+        address got = IZorpFactory(ref_factory).studies(0);
+        vm.assertEq(expected, got, "Unexpected: `ZorpFactory.studies(0)`");
+    }
+
+    function test_read__ref_factory_previous() public view {
+        address expected = address(0);
+        address got = IZorpFactory(ref_factory).ref_factory_previous();
+        vm.assertEq(expected, got, "Unexpected: `ZorpFactory.ref_factory_previous()`");
+    }
+
+    function test_read__ref_factory_next() public view {
+        address expected = address(0);
+        address got = IZorpFactory(ref_factory).ref_factory_next();
+        vm.assertEq(expected, got, "Unexpected: `ZorpFactory.ref_factory_next()`");
+    }
+}
+
+contract ZorpFactory_FakeUpdate is ZorpFactory {
+    constructor (address payable initialOwner_, address _ref_factory_previous) ZorpFactory(initialOwner_) {
+        ref_factory_previous = _ref_factory_previous;
+    }
+}
+
+/// @dev see: https://book.getfoundry.sh/cheatcodes/expect-emit
+contract ZorpFactory_Success_Write_Test is Test {
+    address payable immutable ZORP_FACTORY__OWNER;
+    address ref_factory;
+
+    constructor() {
+        ZORP_FACTORY__OWNER = payable(address(this));
+    }
+
+    function setUp() public {
+        vm.deal(address(this), 1000 ether);
+        // Deploy a fresh factory for each test
+        ref_factory = address(new ZorpFactory(ZORP_FACTORY__OWNER));
+    }
+
+    function test_write__createStudy() public {
+        address payable initialOwner = payable(address(68));
+        string memory encryptionKey = "wat";
+        uint256 msg_value = 1;
+
+        // Note: knowing the emit-ed address ahead of time is not feasible
+        vm.expectEmit(false, false, false, true, ref_factory);
+        emit StudyCreated(address(1336));
+        address ref_study = IZorpFactory(ref_factory).createStudy{ value: msg_value }(initialOwner, encryptionKey);
+
+        uint256 index_study = IZorpFactory(ref_factory).latest_study_index();
+        address expected_study_ref = IZorpFactory(ref_factory).studies(index_study);
+        vm.assertEq(expected_study_ref, ref_study, "Unexpected: `ZorpFactory.studies(index_study)`");
+    }
+
+    function test_write__setRefFactoryNext() public {
+        address new_ref_factory = address(new ZorpFactory_FakeUpdate(ZORP_FACTORY__OWNER, ref_factory));
+
+        vm.expectEmit(true, true, true, true, ref_factory);
+        emit FactoryUpdated(ref_factory, new_ref_factory);
+        IZorpFactory(ref_factory).setRefFactoryNext(new_ref_factory);
+
+        vm.assertEq(new_ref_factory, IZorpFactory(ref_factory).ref_factory_next(), "Unexpected: `ZorpFactory.ref_factory_next()`");
+
+        vm.assertEq(address(0), IZorpFactory(new_ref_factory).ref_factory_next(), "Unexpected: `ZorpFactory_FakeUpdate.ref_factory_next()`");
+        vm.assertEq(ref_factory, IZorpFactory(new_ref_factory).ref_factory_previous(), "Unexpected: `ZorpFactory_FakeUpdate.ref_factory_previous()`");
+    }
+
+    function test_write__withdraw() public {
+        address payable to = payable(address(0xb0ba7ea));
+        uint256 amount = 41968;
+
+        (bool paid, ) = ref_factory.call{ value: amount }("");
+
+        IZorpFactory(ref_factory).withdraw(to, amount);
+
+        vm.assertTrue(paid, "Unexpected: `paid` value from `ref_factory.call{ value: amount }(\"\")`");
+        vm.assertEq(amount, address(to).balance, "Unexpected: `ZorpFactory.withdraw(to, amount)`");
+    }
+
+    function test_write__paginateSubmittedData() public {
+        address payable initialOwner = payable(address(68));
+        string memory encryptionKey = "wat";
+        uint256 msg_value = 1;
+
+        address ref_study = IZorpFactory(ref_factory).createStudy{ value: msg_value }(initialOwner, encryptionKey);
+
+        string[] memory results = new string[](10);
+        results = IZorpFactory(ref_factory).paginateSubmittedData(ref_study, 1, 10);
+
+        for (uint256 i; i < results.length;) {
+            vm.assertEq("", results[i], "Unexpected: `results[i]` from `ZorpFactory.paginateSubmittedData(ref_study, 1, 10)`");
+            unchecked { ++i; }
+        }
+    }
+
+    function test_write__paginateStudies() public {
+        address payable initialOwner = payable(address(68));
+        string memory encryptionKey = "wat";
+        uint256 msg_value = 1;
+
+        address[] memory ref_studies_expected = new address[](10);
+        for (uint256 i; i < ref_studies_expected.length; ) {
+            ref_studies_expected[i] = IZorpFactory(ref_factory).createStudy{ value: msg_value }(initialOwner, encryptionKey);
+            unchecked { ++i; }
+        }
+
+        address[] memory results = new address[](10);
+        results = IZorpFactory(ref_factory).paginateStudies(1, 10);
+
+        for (uint i; i < results.length;) {
+            vm.assertEq(ref_studies_expected[i], results[i], "Unexpected: `results[i]` from `ZorpFactory(ref_factory).paginateStudies(1, 10)`");
+            unchecked { ++i; }
+        }
+    }
+}
+
---
+++ zorp-contracts/test/ZorpStudy_Success.t.sol
@@ -0,0 +1,318 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { Test } from "forge-std/Test.sol";
+
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+import { IZorpStudy } from "../src/IZorpStudy.sol";
+import { ZorpStudy } from "../src/ZorpStudy.sol";
+
+contract ZorpStudy_Success_Read_Test is Test {
+    address payable immutable ZORP_STUDY__OWNER;
+    address payable immutable ZORP_STUDY__PARTICIPANT;
+    string ZORP_STUDY__IPFS_CID;
+    string ZORP_STUDY__DATA;
+    address ref_study;
+
+    constructor() {
+        ZORP_STUDY__OWNER = payable(address(this));
+        ZORP_STUDY__PARTICIPANT = payable(address(this));
+        ZORP_STUDY__IPFS_CID = "0xDEADBEEF";
+    }
+
+    function setUp() public {
+        vm.deal(address(this), 1000 ether);
+        // Deploy a fresh study for each test
+        uint256 amount = 1 ether;
+        ref_study = address(new ZorpStudy{ value: amount }(ZORP_STUDY__OWNER, ZORP_STUDY__IPFS_CID));
+    }
+
+    function test_read__creator() public view {
+        vm.assertEq(address(this), IZorpStudy(ref_study).creator(), "Unexpected: `ZorpStudy.creator()`");
+    }
+
+    function test_read__submissions() public view {
+        vm.assertEq(0, IZorpStudy(ref_study).submissions(), "Unexpected: `ZorpStudy.submissions()`");
+    }
+
+    function test_read__invalidated() public view {
+        vm.assertEq(0, IZorpStudy(ref_study).invalidated(), "Unexpected: `ZorpStudy.invalidated()`");
+    }
+
+    function test_read__study_status() public view {
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__NA(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    function test_read__participant_payout_amount() public view {
+        vm.assertEq(0, IZorpStudy(ref_study).participant_payout_amount(), "Unexpected: `ZorpStudy.participant_payout_amount()`");
+    }
+
+    function test_read__encryption_key() public view {
+        vm.assertEq(ZORP_STUDY__IPFS_CID, IZorpStudy(ref_study).encryption_key(), "Unexpected: `ZorpStudy.encryption_key()`");
+    }
+
+    function test_read__participant_status() public view {
+        vm.assertEq(IZorpStudy(ref_study).PARTICIPANT_STATUS__NA(), IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT), "Unexpected: `ZorpStudy.participant_status(ZORP_STUDY__PARTICIPANT)`");
+    }
+
+    function test_read__participant_index() public view {
+        vm.assertEq(0, IZorpStudy(ref_study).participant_index(ZORP_STUDY__PARTICIPANT), "Unexpected: `ZorpStudy.participant_index(ZORP_STUDY__PARTICIPANT)`");
+    }
+
+    function test_read__submitted_data() public view {
+        uint256 index = 0;
+        vm.assertEq("", IZorpStudy(ref_study).submitted_data(index), "Unexpected: `ZorpStudy.submitted_data(index)`");
+    }
+}
+
+/// Used for generating new addresses that can interact with target ZorpStudy instance
+contract ZorpStudy_SubmitterProxy is Ownable, ReentrancyGuard {
+    constructor (address initialOwner_) payable Ownable(initialOwner_) { }
+
+    function submitData(address ref_study, string memory ipfs_cid) external payable {
+        IZorpStudy(ref_study).submitData(ipfs_cid);
+    }
+
+    function claimReward(address ref_study) external payable {
+        IZorpStudy(ref_study).claimReward();
+    }
+
+    function withdraw(address payable to, uint256 amount) external payable nonReentrant onlyOwner {
+        (bool success, ) = to.call{value: amount}("");
+        require(success, "ZorpFactory: Failed withdraw");
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+}
+
+contract ZorpStudy_Success_Write_Test is Test {
+    address payable immutable ZORP_STUDY__OWNER;
+    address payable immutable ZORP_STUDY__PARTICIPANT;
+    string ZORP_STUDY__IPFS_CID;
+    string ZORP_STUDY__DATA;
+    address ref_study;
+
+    constructor() {
+        ZORP_STUDY__OWNER = payable(address(this));
+        ZORP_STUDY__PARTICIPANT = payable(address(this));
+        ZORP_STUDY__IPFS_CID = "0xDEADBEEF";
+        ZORP_STUDY__DATA = "0xCAFEBABE";
+    }
+
+    function setUp() public {
+        vm.deal(address(this), 1000 ether);
+        // Deploy a fresh study for each test
+        uint256 amount = 1 ether;
+        ref_study = address(new ZorpStudy{ value: amount }(ZORP_STUDY__OWNER, ZORP_STUDY__IPFS_CID));
+    }
+
+    function test_write__submitData() public {
+        IZorpStudy(ref_study).startStudy();
+
+        IZorpStudy(ref_study).submitData(ZORP_STUDY__DATA);
+
+        uint256 index_submitter = IZorpStudy(ref_study).participant_index(address(this));
+
+        vm.assertEq(1, index_submitter, "Unexpected: `IZorpStudy(ref_study).participant_index(address(this))`");
+        vm.assertEq(1, IZorpStudy(ref_study).submissions(), "Unexpected: `ZorpStudy.submissions()`");
+        vm.assertEq(ZORP_STUDY__DATA, IZorpStudy(ref_study).submitted_data(index_submitter), "Unexpected: `IZorpStudy(ref_study).submitted_data(index_submitter)`");
+        vm.assertEq(IZorpStudy(ref_study).PARTICIPANT_STATUS__SUBMITTED(), IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT), "Unexpected: `IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT)`");
+    }
+
+    function test_write__claimReward() public {
+        IZorpStudy(ref_study).startStudy();
+
+        IZorpStudy(ref_study).submitData(ZORP_STUDY__DATA);
+
+        IZorpStudy(ref_study).endStudy();
+
+        uint256 submissions = IZorpStudy(ref_study).submissions();
+        vm.assertEq(1, submissions, "Unexpected: `IZorpStudy(ref_study).submissions()`");
+
+        uint256 invalidated = IZorpStudy(ref_study).invalidated();
+        vm.assertEq(0, invalidated, "Unexpected: `IZorpStudy(ref_study).invalidated()`");
+
+        uint256 participant_payout_amount_expected = ref_study.balance / (submissions - invalidated);
+        vm.assertEq(participant_payout_amount_expected, IZorpStudy(ref_study).participant_payout_amount(), "Unexpected: `IZorpStudy(ref_study).participant_payout_amount()`");
+
+        vm.assertEq(IZorpStudy(ref_study).PARTICIPANT_STATUS__SUBMITTED(), IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT), "Unexpected: `IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT)`");
+        IZorpStudy(ref_study).claimReward();
+        vm.assertEq(IZorpStudy(ref_study).PARTICIPANT_STATUS__PAID(), IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT), "Unexpected: `IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT)`");
+
+        vm.assertEq(0, ref_study.balance, "Unexpected: `ref_study.balance`");
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__FINISHED(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    function test_write__claimReward__with_terminating_fractional_submissions() public {
+        IZorpStudy(ref_study).startStudy();
+
+        uint256 amount_of_submitters = 10;
+        ZorpStudy_SubmitterProxy[] memory submiter_proxies = new ZorpStudy_SubmitterProxy[](amount_of_submitters);
+        for (uint256 i; i < amount_of_submitters; ) {
+            ZorpStudy_SubmitterProxy submiter_proxy = new ZorpStudy_SubmitterProxy(ZORP_STUDY__PARTICIPANT);
+            submiter_proxies[i] = submiter_proxy;
+
+            submiter_proxy.submitData(ref_study, ZORP_STUDY__DATA);
+
+            unchecked { ++i; }
+        }
+
+        uint256 owner_balance_before = ZORP_STUDY__OWNER.balance;
+        IZorpStudy(ref_study).endStudy();
+        uint256 owner_balance_after = ZORP_STUDY__OWNER.balance;
+
+        vm.assertTrue(owner_balance_before == owner_balance_after, "Unexpected: `ZORP_STUDY__OWNER.balance`");
+
+        uint256 submissions = IZorpStudy(ref_study).submissions();
+        vm.assertEq(amount_of_submitters, submissions, "Unexpected: `IZorpStudy(ref_study).submissions()`");
+
+        uint256 invalidated = IZorpStudy(ref_study).invalidated();
+        vm.assertEq(0, invalidated, "Unexpected: `IZorpStudy(ref_study).invalidated()`");
+
+        uint256 participant_payout_amount_expected = ref_study.balance / (submissions - invalidated);
+        vm.assertEq(participant_payout_amount_expected, IZorpStudy(ref_study).participant_payout_amount(), "Unexpected: `IZorpStudy(ref_study).participant_payout_amount()`");
+
+        for (uint256 i; i < amount_of_submitters; ) {
+            submiter_proxies[i].claimReward(ref_study);
+            vm.assertEq(participant_payout_amount_expected, address(submiter_proxies[i]).balance, "Unexpected: `address(submiter_proxies[i]).balance`");
+
+            unchecked { ++i; }
+        }
+
+        vm.assertEq(0, ref_study.balance, "Unexpected: `ref_study.balance`");
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__FINISHED(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    function test_write__claimReward__with_rational_fractional_submissions() public {
+        IZorpStudy(ref_study).startStudy();
+
+        uint256 amount_of_submitters = 9;
+        ZorpStudy_SubmitterProxy[] memory submiter_proxies = new ZorpStudy_SubmitterProxy[](amount_of_submitters);
+        for (uint256 i; i < amount_of_submitters; ) {
+            ZorpStudy_SubmitterProxy submiter_proxy = new ZorpStudy_SubmitterProxy(ZORP_STUDY__PARTICIPANT);
+            submiter_proxies[i] = submiter_proxy;
+
+            submiter_proxy.submitData(ref_study, ZORP_STUDY__DATA);
+
+            unchecked { ++i; }
+        }
+
+        uint256 owner_balance_before = ZORP_STUDY__OWNER.balance;
+        IZorpStudy(ref_study).endStudy();
+        uint256 owner_balance_after = ZORP_STUDY__OWNER.balance;
+
+        vm.assertTrue(owner_balance_before < owner_balance_after, "Unexpected: `ZORP_STUDY__OWNER.balance`");
+
+        uint256 submissions = IZorpStudy(ref_study).submissions();
+        vm.assertEq(amount_of_submitters, submissions, "Unexpected: `IZorpStudy(ref_study).submissions()`");
+
+        uint256 invalidated = IZorpStudy(ref_study).invalidated();
+        vm.assertEq(0, invalidated, "Unexpected: `IZorpStudy(ref_study).invalidated()`");
+
+        uint256 participant_payout_amount_expected = ref_study.balance / (submissions - invalidated);
+        vm.assertEq(participant_payout_amount_expected, IZorpStudy(ref_study).participant_payout_amount(), "Unexpected: `IZorpStudy(ref_study).participant_payout_amount()`");
+
+        for (uint256 i; i < amount_of_submitters; ) {
+            submiter_proxies[i].claimReward(ref_study);
+            vm.assertEq(participant_payout_amount_expected, address(submiter_proxies[i]).balance, "Unexpected: `address(submiter_proxies[i]).balance`");
+
+            unchecked { ++i; }
+        }
+
+        vm.assertEq(0, ref_study.balance, "Unexpected: `ref_study.balance`");
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__FINISHED(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    function test_write__flagInvalidSubmission() public {
+        IZorpStudy(ref_study).startStudy();
+        IZorpStudy(ref_study).submitData(ZORP_STUDY__DATA);
+
+        IZorpStudy(ref_study).flagInvalidSubmission(address(this));
+
+        vm.assertEq(IZorpStudy(ref_study).PARTICIPANT_STATUS__INVALID(), IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT), "Unexpected: `IZorpStudy(ref_study).participant_status(ZORP_STUDY__PARTICIPANT)`");
+        vm.assertEq(0, IZorpStudy(ref_study).participant_index(ZORP_STUDY__PARTICIPANT), "Unexpected: `IZorpStudy(ref_study).participant_index(ZORP_STUDY__PARTICIPANT)`");
+        vm.assertEq("", IZorpStudy(ref_study).submitted_data(1), "Unexpected: `IZorpStudy(ref_study).submitted_data(1)`");
+        vm.assertEq(1, IZorpStudy(ref_study).invalidated(), "Unexpected: `ZorpStudy.invalidated()`");
+
+        vm.assertEq(1, IZorpStudy(ref_study).submissions(), "Unexpected: `ZorpStudy.submissions()`");
+    }
+
+    function test_write__startStudy() public {
+        IZorpStudy(ref_study).startStudy();
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__ACTIVE(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    function test_write__endStudy__with_zero_submissions() public {
+        IZorpStudy(ref_study).startStudy();
+
+        uint256 owner_balance_before = ZORP_STUDY__OWNER.balance;
+        IZorpStudy(ref_study).endStudy();
+        uint256 owner_balance_after = ZORP_STUDY__OWNER.balance;
+
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__FINISHED(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+        vm.assertTrue(owner_balance_before < owner_balance_after, "Unexpected: `ZORP_STUDY__OWNER.balance`");
+        vm.assertEq(0, ref_study.balance, "Unexpected: `ref_study.balance`");
+    }
+
+    function test_write__endStudy__with_terminating_fractional_submissions() public {
+        IZorpStudy(ref_study).startStudy();
+
+        uint256 amount_of_submitters = 10;
+        ZorpStudy_SubmitterProxy[] memory submiter_proxies = new ZorpStudy_SubmitterProxy[](amount_of_submitters);
+        for (uint256 i; i < amount_of_submitters; ) {
+            ZorpStudy_SubmitterProxy submiter_proxy = new ZorpStudy_SubmitterProxy(ZORP_STUDY__PARTICIPANT);
+            submiter_proxies[i] = submiter_proxy;
+
+            submiter_proxy.submitData(ref_study, ZORP_STUDY__DATA);
+
+            unchecked { ++i; }
+        }
+
+        IZorpStudy(ref_study).endStudy();
+
+        uint256 submissions = IZorpStudy(ref_study).submissions();
+        vm.assertEq(amount_of_submitters, submissions, "Unexpected: `IZorpStudy(ref_study).submissions()`");
+
+        uint256 invalidated = IZorpStudy(ref_study).invalidated();
+        vm.assertEq(0, invalidated, "Unexpected: `IZorpStudy(ref_study).invalidated()`");
+
+        uint256 participant_payout_amount_expected = ref_study.balance / (submissions - invalidated);
+        vm.assertEq(participant_payout_amount_expected, IZorpStudy(ref_study).participant_payout_amount(), "Unexpected: `IZorpStudy(ref_study).participant_payout_amount()`");
+
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__FINISHED(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    function test_write__endStudy__with_rational_fractional_submissions() public {
+        IZorpStudy(ref_study).startStudy();
+
+        uint256 amount_of_submitters = 9;
+        ZorpStudy_SubmitterProxy[] memory submiter_proxies = new ZorpStudy_SubmitterProxy[](amount_of_submitters);
+        for (uint256 i; i < amount_of_submitters; ) {
+            ZorpStudy_SubmitterProxy submiter_proxy = new ZorpStudy_SubmitterProxy(ZORP_STUDY__PARTICIPANT);
+            submiter_proxies[i] = submiter_proxy;
+
+            submiter_proxy.submitData(ref_study, ZORP_STUDY__DATA);
+
+            unchecked { ++i; }
+        }
+
+        IZorpStudy(ref_study).endStudy();
+
+        uint256 submissions = IZorpStudy(ref_study).submissions();
+        vm.assertEq(amount_of_submitters, submissions, "Unexpected: `IZorpStudy(ref_study).submissions()`");
+
+        uint256 invalidated = IZorpStudy(ref_study).invalidated();
+        vm.assertEq(0, invalidated, "Unexpected: `IZorpStudy(ref_study).invalidated()`");
+
+        uint256 participant_payout_amount_expected = ref_study.balance / (submissions - invalidated);
+        vm.assertEq(participant_payout_amount_expected, IZorpStudy(ref_study).participant_payout_amount(), "Unexpected: `IZorpStudy(ref_study).participant_payout_amount()`");
+
+        vm.assertEq(IZorpStudy(ref_study).STUDY_STATUS__FINISHED(), IZorpStudy(ref_study).study_status(), "Unexpected: `ZorpStudy.study_status()`");
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+}
